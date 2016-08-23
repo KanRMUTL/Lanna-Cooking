d64785c23501b6933beeae80ab9dbab2f6272f34
Kiadtisuk <kan_lowongsa@hotmail.com>
2016-08-23 17:02:49 +0700

    *จัดหน้า

--- LannaCookingV.3/app/src/main/java/com/kan/lannacookingv3/MainActivity.java
+++ LannaCookingV.3/app/src/main/java/com/kan/lannacookingv3/MainActivity.java
@@ -1,11 +1,13 @@
 package com.kan.lannacookingv3;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private ListView listView;
@@ -13,8 +15,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Typeface mytypeface = Typeface.createFromAsset(getAssets(), "THSarabun Bold.ttf");
 
         listView = (ListView)findViewById(R.id.listView);
+
+        TextView title = (TextView)findViewById(R.id.title);
+        title.setTypeface(mytypeface);
 
         CookingDAO cookingDAO = new CookingDAO(this);
         final CookingAdapter adapter = new CookingAdapter(this, cookingDAO.findAll());
@@ -29,5 +35,6 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(e);
             }
         });
+
     }
 }
